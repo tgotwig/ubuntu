cdef8dedd519b20a8a926561272f9313c5de6372
Thomas Gotwig <ramble.clef0y@icloud.com>
2024-12-12 21:39:34 +0100

    Remove versions of apt packages

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ set -e
 
 apt update && apt install -y --no-install-recommends \
   ca-certificates \
-  curl=8.5.0-2ubuntu10.4 \
+  curl \
   fish \
-  iputils-ping=3:20240117-1build1 \
-  netcat-openbsd=1.226-1ubuntu2 \
+  iputils-ping \
+  netcat-openbsd \
 
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 echo 'fish_add_path /root/.local/bin' > /etc/fish/config.fish
