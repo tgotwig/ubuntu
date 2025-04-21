b8ab0eb858d67781009d27d1302a732ad73b39e6
Thomas Gotwig <ramble.clef0y@icloud.com>
2025-04-21 10:04:02 +0200

    Add asdf

--- install.sh
+++ install.sh
@@ -22,4 +22,14 @@ echo 'starship init fish | source' >> /etc/fish/config.fish
 
 sh -c "$(curl -sSL https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin
 
+arch="$(uname -m)"
+case "$arch" in
+  x86_64) asdf_arch="amd64" ;;
+  aarch64) asdf_arch="arm64" ;;
+  *) echo "Unsupported architecture: $arch"; exit 1 ;;
+esac
+curl -sSfL "https://github.com/asdf-vm/asdf/releases/download/v0.16.7/asdf-v0.16.7-linux-${asdf_arch}.tar.gz" \
+  | tar -xzf - -C /usr/local/bin
+echo 'asdf completion fish | source' >> /etc/fish/config.fish
+
 rm -rf /var/lib/apt/lists/*
