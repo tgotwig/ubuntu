6c9a08f1dea46658ec215bb02002a10aa2e6e27d
Thomas Gotwig <ramble.clef0y@icloud.com>
2025-12-08 00:02:04 +0100

    Add test recipe

---
+++ script/test.sh
@@ -0,0 +1,23 @@
+set -e
+
+export PATH="/root/.local/bin:$PATH"
+
+echo "[asdf]" && asdf -v
+echo "[cc]" && cc --version
+echo "[curl]" && curl -V
+echo "[dig]" && dig -v
+echo "[nslookup]" && nslookup -version
+echo "[fish]" && fish -v
+echo "[ip]" && ip -V
+echo "[jq]" && jq --version
+echo "[less]" && less -V
+echo "[mediainfo]" && mediainfo --Version
+echo "[micro]" && micro -version
+echo "[nc]" && nc -h 2>&1 | head -1
+echo "[nmap]" && nmap --version | head -2
+echo "[ping]" && ping -V
+echo "[starship]" && starship --version
+echo "[task]" && task --version
+echo "[tree]" && tree --version
+echo "[wget]" && wget --version | head -1
+echo "[z]" && zoxide --version
